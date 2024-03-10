5e9ebe5eaa40dd4a8b0498d3edc4716221e881a1
Tisolaso <spanya2017@gmail.com>
2024-03-10 12:08:57 -0700

    initial implementation v2

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Create Review """
+    place_id = ""
+    user_id = ""
+    text = ""
+
+
+"""
+    def __init__(self, place_id, user_id):
+        Review.place_id = place_id
+        Review.user_id = user_id
+"""
