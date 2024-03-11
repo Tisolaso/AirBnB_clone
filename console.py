280043455a06d2ba0d7efc4f79cc2cb1c77ce27d
Tisolaso <spanya2017@gmail.com>
2024-03-11 15:28:01 -0700

    final clone

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Console module """
+""" Console module for AirBnB """
 import cmd
 from models.base_model import BaseModel
 from models.user import User
--- models/city.py
+++ models/city.py
@@ -3,9 +3,10 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ Create a city """
+    """ DOC DOC DOC """
     state_id = ""
     name = ""
+
 
 """    def __init__(self, state_id):
         City.state_id = state_id
